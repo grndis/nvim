ccaea0473dc6a9a51e95ce438ea63dbb392ebbfe
Grandis SYF <gra@dr.com>
2024-06-28 21:44:34 +0700

    setup

--- lua/community.lua
+++ lua/community.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -8,5 +8,14 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
+  { import = "astrocommunity.completion.copilot-lua" },
+  { import = "astrocommunity.utility.noice-nvim" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.php" },
+  { import = "astrocommunity.recipes.neovide" },
+  { import = "astrocommunity.recipes.heirline-vscode-winbar" },
+  { import = "astrocommunity.recipes.telescope-nvchad-theme" },
   -- import/override with your plugins folder
 }
--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroUI provides the basis for configuring the AstroNvim User Interface
 -- Configuration documentation can be found with `:h astroui`
@@ -19,6 +19,12 @@ return {
       },
       astrodark = { -- a table of overrides/changes when applying the astrotheme theme
         -- Normal = { bg = "#000000" },
+        Normal = { bg = "#000000" },
+        NormalFloat = { bg = "#000000" },
+        NormalNC = { bg = "#000000" },
+        SignColumn = { bg = "#000000" },
+        StatusLine = { bg = "#000000" },
+        VertSplit = { bg = "#000000" },
       },
     },
     -- Icons can be configured throughout the interface

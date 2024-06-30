f038d1889dcb2430340cf64e7878527ca34cd2db
Grandis SYF <gra@dr.com>
2024-06-30 12:58:14 +0700

    add plugin

--- lua/community.lua
+++ lua/community.lua
@@ -9,7 +9,7 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.completion.copilot-cmp" },
   { import = "astrocommunity.utility.noice-nvim" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.vue" },
@@ -19,9 +19,10 @@ return {
   { import = "astrocommunity.recipes.telescope-nvchad-theme" },
   { import = "astrocommunity.color.modes-nvim" },
   { import = "astrocommunity.indent.indent-rainbowline" },
-  { import = "astrocommunity.scrolling.neoscroll-nvim" },
+  { import = "astrocommunity.scrolling.vim-smoothie" },
   { import = "astrocommunity.scrolling.nvim-scrollbar" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
+  -- { import = "astrocommunity.keybinding.nvcheatsheet-nvim" },
   -- { import = "astrocommunity.terminal-integration.vim-tmux-navigator" },
   -- import/override with your plugins folder
 }
